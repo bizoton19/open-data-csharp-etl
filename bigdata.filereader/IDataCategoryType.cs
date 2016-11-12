09ddb7f7f77c909045beb730120ad9847181a902
Alexandre Salomon <bizoton19@gmail.com>
2016-11-12 18:44:18 -0500

    initial commit

---
+++ bigdata.filereader/IDataCategoryType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bigdata.filereader
+{
+    interface IDataCategoryType
+    {
+     string Type { get; set; }
+    }
+}
